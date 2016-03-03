ddb5fdb475e61b4763de30ef0d291b6024f2f6c3
Dylan Porteus <dylanporteus@aol.com>
2016-03-03 15:38:27 -0500

    complete basic styling and being work on responsiveness

--- public/js/text-translation.js
+++ public/js/text-translation.js
@@ -4,13 +4,24 @@
    console.log('text_translation.js loaded.')
 	$('.form-button').click(function(){
         var engInput = $('.form-input').val(); 
-        
+        $('audio').length && $('audio').remove();
         $.getJSON(`/translate?text=${engInput}`, (data) => {
         	let translation = data.translation;
         	$('#result-ita').text(translation);
+        	let audio_element = $(
+        		//The below code block is being passed to the client side html
+        		//to implement the autoplay feature of an audio element.
+				`
+				<audio style="display:none;" autoplay>
+				 <source src="/text-to-speech.ogg?text=${translation}" type="audio/mpeg">
+				</audio>
+				`
+        	);
+
+        	$(audio_element).appendTo('body')
         });
 
-        return false;
+        return false;  //This line serves the same purpose as .eventPreventDefault()
     })
 
 })();
